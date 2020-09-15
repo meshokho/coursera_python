bf3dfa115aa31f640606db4eef7ee840aa56a80e
meshokho <meshokho@gmail.com>
2020-09-15 11:52:28 +0300

    added square_eq

---
+++ Course1/Week1/square_eq.py
@@ -0,0 +1,11 @@
+import sys
+from math import sqrt
+a = int(sys.argv[1])
+b = int(sys.argv[2])
+c = int(sys.argv[3])
+
+d = b*b - 4*a*c
+x1 = (-b + sqrt(d) )/ (2*a)
+x2 = (-b - sqrt(d)) / (2*a)
+print(int(x1))
+print(int(x2))
